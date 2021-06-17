84e9c25c4a52eed8459d3a60aa651c4d551b6dc6
Hiruni06 <56539533+Hiruni06@users.noreply.github.com>
2021-06-17 15:38:56 +0530

    login done

---
+++ frontend/src/pages/customer/Login.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state={
+
+            email: '',
+            password: ''
+
+        };
+    }
+
+    handleEmailChange =(event)=> {
+        this.setState({
+            email: event.target.value
+        })
+    }
+
+    handlePasswordChange =(event)=>{
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    handleSubmit = (event)=>{
+        
+    }
+
+    render() {
+        return (
+            <div class="p-25 w-2/4 items-center mt-20 ml-40 mr-60 shadow-inner">
+            <form class='bg-white shadow-lg rounded px-8 pt-6 pb-8 mb-4' 
+            onSubmit={this.handleSubmit}>
+                <div class='mb-4'>
+                    <label class='block mb-2 text-md font-bold text-gray-700'>
+                        Email
+                    </label>
+                    <input class='w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none focus:shadow-outline' 
+                    type="text" placeholder="email" 
+                    value={this.state.email} 
+                    onChange={this.handleEmailChange}/>
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 text-md font-bold text-gray-700">
+                        Password
+                    </label>
+                    <input class="w-full px-3 py-2 mb-3 leading-tight text-gray-700 border rounded shadow appearance-none border-500 focus:outline-none focus:shadow-outline" 
+                    type="password" placeholder="password"
+                    value={this.state.password}
+                    onChange={this.handlePasswordChange}/>
+                </div>
+
+                <div class="flex items-center justify-center">
+                <a class="inline-block text-md font-bold text-green-500 align-baseline hover:text-green-800" href="#">
+                        Forgot Password?
+                    </a>
+                </div>
+
+                <div class="flex items-center justify-center mt-4">
+                    <button class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline" type="submit">
+                        Sign In
+                    </button>
+                    
+                </div>
+                
+
+                <div class>
+                <a class="inline-block text-md font-bold text-green-800 align-baseline hover:text-green-1000" href="#">
+                    If you don't have an account? Sign Up
+                    </a>
+                </div>
+            </form>
+         </div>
+
+        );
+    }
+}
+
+export default Login;
