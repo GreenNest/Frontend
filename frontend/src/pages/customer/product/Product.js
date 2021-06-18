62e72234b066d7fb0347b346c1b693a1db0421aa
sulakshanee <sulakshanee111@gmail.com>
2021-06-18 09:52:12 +0530

    after create reviews

--- frontend/src/pages/customer/product/Product.js
+++ frontend/src/pages/customer/product/Product.js
@@ -1,12 +1,15 @@
-import img1 from './mango-tree.jpg'
+import Review from './Review'
+
+import img1 from '../../../assets/product_img/mango-tree.jpg'
+import mango1 from '../../../assets/product_img/mango1.jpg'
 
 const Product = () => {
     return (
-        <div className="min-w-full bg-red-400 lg:p-36 md:min-w-0 sm:p-20">
-            <div className="bg-blue-300 md:flex">
-                <div className="lg:w-5/12 md:w-full">
-                    <img className="rounded-xl" src={img1} alt="Main Image"/>
-                    <div className="grid grid-cols-3 mt-12 justify-items-start">
+        <div className="min-w-full md:min-w-0 sm:p-20 lg:px-32">
+            <div className="shadow-xl md:flex rounded-xl">
+                <div className="lg:w-6/12">
+                    <img className="min-w-full" src={mango1} alt="Main Image"/>
+                    <div className="flex flex-row flex-wrap justify-center py-3 mt-5 mb-5 space-x-8 border-t-2 border-b-2 border-gray-300">
                         <img className="w-32 h-auto" src={img1} alt="Main Image"/>
                         <img className="w-32 h-auto" src={img1} alt="Main Image"/>
                         <img className="w-32 h-auto" src={img1} alt="Main Image"/>
@@ -14,13 +17,13 @@ const Product = () => {
                 </div>
                 <div className="lg:ml-10 sm:mt-10 sm:mx-5">
                     <h1 className="font-sans text-4xl font-bold">Red Mango Plant | F701</h1>
-                    <p className="mt-3 text-2xl font-semibold font-moon">Rs. 3500.00</p>
+                    <p className="mt-3 text-2xl font-semibold font-moon">Rs. 250.00</p>
                     <div className="mt-10 text-lg font-normal">
                         <p>- FREE Delivery (Kurunagala District only)</p>
                         <p>- Payment Methods : Cash on Delivery / Online Payement</p>
                     </div>
                     <div className="mt-16">
-                        <p className="text-lg font-semibold">30 in stock</p>
+                        <p className="text-lg font-semibold text-lightgreen">30 in stock</p>
                         <form className="flex mt-5 justify-items-start">
                             <div>
                                 <input type="number" className="w-10 h-10 text-2xl text-center rounded" value="1"/>
@@ -30,7 +33,7 @@ const Product = () => {
                             <button className="h-10 px-2 font-bold text-white bg-blue-600 rounded hover:bg-blue-700 sm:ml-2 lg:ml-4">REQUEST ORDER</button>
                         </form>
                     </div>
-                    <div className="mt-16 border-t-2 border-fuchsia-600">
+                    <div className="mt-16 border-t-2 border-gray-300">
                         <div className="mt-3">
                             <span className="font-medium text-md">CATEGORIES: </span>
                             <a>Fruit Plants</a>,
@@ -38,6 +41,15 @@ const Product = () => {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className="mt-16">
+                <div className="text-4xl font-bold text-center text-black text-opacity-70">Item Reviews</div>
+                <Review />
+                <Review />
+                <Review />
+            </div>
+            <div className="mt-16">
+                <div className="text-4xl font-bold text-center text-black text-opacity-70">Related Products</div>
             </div>
         </div>
     );
