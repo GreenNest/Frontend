812c79f09ada772e05d9e0f409719c09e01fe26e
Piyumi047 <piyumidulanjali1998@gmail.com>
2021-06-19 15:13:42 +0530

    category admin view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Shop from './pages/customer/Shop/Shop';
 import Index from './pages/customer/index.js';
 import Login from './pages/customer/Login';
 import Product from './pages/customer/product/Product';
+import Categoryadminview from './pages/accountant/categoryadminview/Categoryadminview';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
     <Route exact path="/shop/product" component={Product}/>
     <Route exact path="/profile/order/review" component={Order}/>
     <Route exact path="/profile/orders" component={Orderhistory}/>
+    <Route exact path="/admin/categoryview"  component={Categoryadminview} />
     </Switch>
     </>
   );
