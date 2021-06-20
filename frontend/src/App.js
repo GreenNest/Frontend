cb49abd2164b5adc7e69ef284bfc0420bca57ca5
Piyumi047 <piyumidulanjali1998@gmail.com>
2021-06-20 15:52:04 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,19 +2,15 @@ import React from 'react';
 import './styles/index.css';
 import {BrowserRouter as Router , Route , Switch} from "react-router-dom";
 import Orderhistory from './pages/customer/Orderhistory';
-import Index from './pages/customer/Index';
+import Index from './pages/customer/index';
 import Order from './pages/customer/customerorder/Order';
 import Shop from './pages/customer/Shop/Shop';
 import Login from './pages/customer/Login';
 import Product from './pages/customer/product/Product';
-<<<<<<< HEAD
 import Categoryadminview from './pages/admin/categoryadminview/Categoryadminview';
-import Itemadminview from './pages/admin/itemadminview/Itemadminview';
-=======
-import Categoryadminview from './pages/accountant/categoryadminview/Categoryadminview';
 import Signup from './pages/customer/Signup';
+import Itemadminview from './pages/admin/itemadminview/Itemadminview';
 
->>>>>>> 6caf665936e83d8e60e3d543cdfd402e03559c46
 
 function App() {
   return (
@@ -27,12 +23,9 @@ function App() {
     <Route exact path="/profile/order/review" component={Order}/>
     <Route exact path="/profile/orders" component={Orderhistory}/>
     <Route exact path="/admin/categoryview"  component={Categoryadminview} />
-<<<<<<< HEAD
-    <Route exact path="/admin/itemview"  component={Itemadminview} />
-=======
+    <Route exact path="/admin/categoryview/itemview"  component={Itemadminview} />
     <Route exact path="/signup" component={Signup} />
 
->>>>>>> 6caf665936e83d8e60e3d543cdfd402e03559c46
     </Switch>
     </>
   );
