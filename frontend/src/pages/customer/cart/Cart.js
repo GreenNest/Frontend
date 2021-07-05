87d54f826605587d9c148509b1bd09919c6671b2
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-07-06 01:32:58 +0530

    cart ui updated

--- frontend/src/pages/customer/cart/Cart.js
+++ frontend/src/pages/customer/cart/Cart.js
@@ -6,20 +6,39 @@ import { Link } from "react-router-dom";
 
 const Cart = () => {
     return(
-        <div className="min-w-full md:min-w-0 sm:p-20 lg:px-32">
-                {/* Alert-No added items to display */}
-                <div className="bg-red-100 border border-red-400 text-red-700 px-7 py-3 rounded relative my-5" role="alert">
-                    {/* <strong className="font-bold">Alert!</strong> */}
-                    <span className="block sm:inline">Your cart is empty !</span>
-                </div>
-
-                {/* Item components */}
-                <CartItem />
-                <CartItem />
-                <Link to="shop">
-                    <button className="bg-blue-500 hover:bg-blue-400 text-white flex font-bold py-2 px-4 rounded"><MdAssignmentReturn className="mt-1 mr-1"/> Return to Shop</button>
-                </Link>
+        <div className="min-w-full md:min-w-0 sm:p-20 lg:px-32 mt-0">
+            {/* Alert-No added items to display */}
+            <div className="bg-red-100 border border-red-400 text-red-700 px-7 py-3 rounded relative my-5" role="alert">
+                {/* <strong className="font-bold">Alert!</strong> */}
+                <span className="block sm:inline">Your cart is empty !</span>
             </div>
+
+            <div className="flex max-w-7xl  items-center p-2 rounded-lg mt-2 mb-2 ">
+                {/* Item */}
+                <div className="flex-none mr-2 ml-20">
+                    <button button className="flex font-bold py-2 px-4 rounded border border-grey-500 md:border-grey-500">Item</button>
+                </div>
+                {/* Price */}
+                <div className="flex-none mr-2 ml-10">
+                    <button className="flex font-bold py-2 px-4 rounded border border-grey-500 md:border-grey-500">Price</button>
+                </div>
+                {/* Quantity */}
+                <div className="flex-none mr-2 ml-10">
+                    <button className="flex font-bold py-2 px-4 rounded border border-grey-500 md:border-grey-500">Quantity</button>
+                </div>
+                {/* Subtotal */}
+                <div className="flex-none mr-2 ml-10">
+                    <button button className="flex font-bold py-2 px-4 rounded border border-grey-500 md:border-grey-500">Subtotal</button>
+                </div>
+            </div>
+
+            {/* Item components */}
+            <CartItem />
+            <CartItem />
+            <Link to="shop">
+                <button className="bg-blue-500 hover:bg-blue-400 text-white flex font-bold py-2 px-4 rounded"><MdAssignmentReturn className="mt-1 mr-1"/> Return to Shop</button>
+            </Link>
+        </div>
     );
 }
 
--- frontend/src/pages/customer/cart/CartItem.js
+++ frontend/src/pages/customer/cart/CartItem.js
@@ -4,15 +4,34 @@ import { MdDeleteSweep } from 'react-icons/md';
 
 const CartItem = () => {
     return(
-        <div className="md:flex max-w-7xl mx-auto flex bg-grey-100 p-6 bg-white rounded-lg shadow-lg mt-2 mb-2">
-            <div className="md:flex mr-2">
+        <div className="flex max-w-7xl justify-start items-center bg-gray-100 p-6 rounded-lg mt-2 mb-2 ">
+            {/* Checkbox */}
+            <div className="flex-1 mr-2 ml-5">
                 <input type="checkbox" checked className="checked:bg-blue-600 checked:border-transparent w-5 h-5"></input>
             </div>
-            <div className="w-1/12 md:w-2/12 lg:w-2/12 relative border">
-                <img className="min-w-full" src="./GreenNest.png" alt="item-img"/>
+            {/* image */}
+            <div className="flex-none w-1/12 md:w-2/12 lg:w-2/12 bg-white border-black md:border-black">
+                <img className="w-full " src="./GreenNest.png" alt="item-img"/>
             </div>
-            <div className="md:flex ml-2 w-6/12 md:w-2/12 lg:w-2/12">
-                <MdDeleteSweep className="w-8 h-8"/>
+            {/* Item */}
+            <div className="flex-auto mr-2 ml-6">
+                <button className="flex font-bold py-2 px-4 rounded border bg-white border-black md:border-black">Mango Tree - TK2003</button>
+            </div>
+            {/* Price */}
+            <div className="flex-auto mr-2 ml-2">
+                <button className="flex font-bold py-2 px-4 rounded border bg-white border-black md:border-black">200 LKR</button>
+            </div>
+            {/* Quantity */}
+            <div className="flex-auto mr-2 ml-2">
+                <button className="flex font-bold py-2 px-4 rounded border bg-white border-black md:border-black">3</button>
+            </div>
+            {/* Subtotal */}
+            <div className="flex-auto mr-2 ml-2">
+                <button className="flex font-bold py-2 px-4 rounded border bg-white border-black md:border-black">600 LKR</button>
+            </div>
+            {/* Remove item  */}
+            <div className="flex-1 ml-2 w-6/12 md:w-2/12 lg:w-2/12 mr-0">
+                <MdDeleteSweep className="w-7 h-7"/>
             </div>
         </div>
     );
