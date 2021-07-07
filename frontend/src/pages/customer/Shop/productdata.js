6de1d8d492edce3a7b2ab99f420a8a11dfed5cc2
Piyumi047 <piyumidulanjali1998@gmail.com>
2021-07-07 10:56:13 +0530

    update product data

--- frontend/src/pages/customer/Shop/productdata.js
+++ frontend/src/pages/customer/Shop/productdata.js
@@ -1,127 +1,139 @@
 import plant1 from "../../../assets/plant1.jpg"
 import plant3 from "../../../assets/plant3.jpg"
 import plant2 from "../../../assets/plant2.jpg"
+import cane_plam from "../../../assets/cane_plam.jpg"
+import sensavaria from "../../../assets/sensavaria.jpg"
+import raphis from "../../../assets/raphis.jpg"
+import curry_leaves from "../../../assets/curry_leaves.jpg"
+import b_avacado from "../../../assets/b_avacado.jpg"
+import cristina from "../../../assets/cristina.jpg"
+import rosa_pichcha from "../../../assets/rosa_pichcha.jpg"
+import plastic_pot from "../../../assets/plastic_pot.jpg"
+import nelli from "../../../assets/nelli.jpg"
+import lemmon from "../../../assets/lemmon.jpg"
+ import naimiris from "../../../assets/nai_miris.jpg"
+import rosa from "../../../assets/rosa_flower.jpg"
 
 const product_card =[
     {
         id:1,
-        product_name:"schefflera Nora ",
-        description:"2 in Stock",
-        price:350,
+        product_name:"Cane Plam ",
+        description:"12 in Stock",
+        price:1350,
         currency: "LKR",
         reorderlevel:10,
-        qty:40,
-        thumb:plant1
+        qty:12,
+        thumb:cane_plam
     },
     {
         id:2,
-        product_name:"schefflera Nora",
-        description:"2 in Stock",
+        product_name:"Sensaveria",
+        description:"8 in Stock",
         price:250,
         currency:"LKR",
         reorderlevel:10,
-        qty:40,
-        thumb:plant2
+        qty:8,
+        thumb:sensavaria
     },
     {
         id:3,
-        product_name:"schefflera Nora",
-        description:"2 in Stock",
-        price:450,
+        product_name:"Raphis Plam",
+        description:"5 in Stock",
+        price:1450,
         currency:"LKR",
         reorderlevel:10,
-        qty:40,
-        thumb:plant3
+        qty:5,
+        thumb:raphis
     },
     {
         id:4,
-        product_name:"schefflera Nora",
+        product_name:"Curry Leaves",
         description:"2 in Stock",
         price:350,
         currency:"LKR",
         reorderlevel:10,
         qty:40,
-        thumb:plant1
+        thumb:curry_leaves
     },
     {
         id:5,
-        product_name:"schefflera Nora",
-        description:"2 in Stock",
-        price:250,
+        product_name:"Budded Avcado",
+        description:"10 in Stock",
+        price:450,
         currency:"LKR",
         reorderlevel:10,
-        qty:40,
-        thumb:plant3
+        qty:10,
+        thumb:b_avacado
     },
     {
         id:6,
-        product_name:"schefflera Nora",
-        description:"2 in Stock",
+        product_name:"Cristina",
+        description:"7 in Stock",
         price:450,
         currency:"LKR",
         reorderlevel:10,
-        qty:40,
-        thumb:plant1
+        qty:7,
+        thumb:cristina
     },
     {
         id:7,
-        product_name:"schefflera Nora",
-        description:"2 in Stock",
-        price:350,
+        product_name:"Rosa Pichcha",
+        description:"14 in Stock",
+        price:550,
         currency: "LKR",
         reorderlevel:10,
-        qty:40,
-        thumb:plant1
+        qty:14,
+        thumb:rosa_pichcha
     },
     {
         id:8,
-        product_name:"schefflera Nora",
-        description:"2 in Stock",
-        price:250,
+        product_name:"Plastic Pot",
+        description:"32 in Stock",
+        price:150,
         currency:"LKR",
         reorderlevel:10,
-        qty:40,
-        thumb:plant2
+        qty:32,
+        thumb:plastic_pot
     },
     {
         id:9,
-        product_name:"schefflera Nora",
-        description:"2 in Stock",
+        product_name:"Nelli Plant",
+        description:"14 in Stock",
         price:450,
         currency:"LKR",
         reorderlevel:10,
-        qty:40,
-        thumb:plant3
+        qty:14,
+        thumb:nelli
     },
     {
         id:10,
-        product_name:"schefflera Nora",
-        description:"2 in Stock",
+        product_name:"Lemmon Plant",
+        description:"15 in Stock",
         price:350,
         currency:"LKR",
         reorderlevel:10,
-        qty:40,
-        thumb:plant1
+        qty:15,
+        thumb:lemmon
     },
     {
         id:11,
-        product_name:"schefflera Nora",
-        description:"2 in Stock",
-        price:250,
-        currency:"LKR",
-        reorderlevel:10,
-        qty:40,
-        thumb:plant3
-    },
-    {
-        id:12,
-        product_name:"schefflera Nora",
-        description:"2 in Stock",
+        product_name:"Nai Miris",
+        description:"16 in Stock",
         price:450,
         currency:"LKR",
         reorderlevel:10,
-        qty:40,
-        thumb:plant1
+        qty:16,
+        thumb:naimiris
+    },
+    {
+        id:12,
+        product_name:"Red Rose",
+        description:"20 in Stock",
+        price:850,
+        currency:"LKR",
+        reorderlevel:10,
+        qty:20,
+        thumb:rosa
     }
      
 ]
