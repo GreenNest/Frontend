472c3bfc4859f48fc92415ac8d5d2e46233b2056
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-07-10 23:33:01 +0530

    Invoicehistory.js edited

--- frontend/src/pages/accountant/InvoiceHistory.js
+++ frontend/src/pages/accountant/InvoiceHistory.js
@@ -20,7 +20,7 @@ const InvoiceHistory = ()=>{
 
 
     return (
-        <div class="justify-center p-3 ml-40 z-50 mt-20 mb-20 w-2/4 bg-white shadow-xl">
+        <div class="justify-center p-3 ml-96 z-50 mt-6 mb-20 w-6/12 bg-white shadow-xl">
 
             <h1 class="text-4xl text-center mt-6 font-bold font-sans"> Invoice History </h1>
 
