6b1e0ddf3d3ff4de2252be2545d607efce20d2bc
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-07-13 12:51:30 +0530

    ui edited

--- frontend/src/pages/LeaveStat.js
+++ frontend/src/pages/LeaveStat.js
@@ -5,7 +5,7 @@ class LeaveStat extends Component {
     
     render() {
         return (
-            <div className="relative z-50 w-10/12 p-3 mt-20 mb-20 bg-white shadow-xl ml-36">
+            <div className="relative z-50 w-9/12 p-3 mt-20 mb-20 bg-white shadow-xl ml-72">
 
                 <div class="w-full h-full p-7 rounded-md shadow-lg border-solid border-2"><Barchart/></div>
 
