f5e549fc062a63f045b42db70e996945c405861c
sulakshanee <sulakshanee111@gmail.com>
2021-07-13 16:03:42 +0530

    css changes

--- frontend/src/pages/customer/Shop/Shop.js
+++ frontend/src/pages/customer/Shop/Shop.js
@@ -5,12 +5,12 @@ import Productlist from './Productlist';
 
 const Shop = () => {
     return ( 
-        <div className="flex flex-row h-full">
-            <div className="flex-initial flex-col w-screen md:w-1/6 py-5 bg-gray-100 visible md:visible">
+        <div className="flex flex-row h-full bg-gray-100">
+            <div className="flex-col flex-initial visible w-screen md:w-1/6 md:visible">
                 <Category />
             </div>
 
-            <div className="flex flex-col w-5/6  py-5 box-border bg-gray-100">
+            <div className="box-border flex flex-col w-5/6 mb-10">
                 <Productlist />               
             </div>
         </div>
--- frontend/src/pages/customer/Shop/productdata.js
+++ frontend/src/pages/customer/Shop/productdata.js
@@ -1,64 +1,68 @@
-import plant1 from "../../../assets/plant1.jpg"
-import plant3 from "../../../assets/plant3.jpg"
-import plant2 from "../../../assets/plant2.jpg"
-import cane_plam from "../../../assets/cane_plam.jpg"
-import sensavaria from "../../../assets/sensavaria.jpg"
-import raphis from "../../../assets/raphis.jpg"
 import curry_leaves from "../../../assets/curry_leaves.jpg"
 import b_avacado from "../../../assets/b_avacado.jpg"
 import cristina from "../../../assets/cristina.jpg"
 import rosa_pichcha from "../../../assets/rosa_pichcha.jpg"
 import plastic_pot from "../../../assets/plastic_pot.jpg"
-import nelli from "../../../assets/nelli.jpg"
+
 import lemmon from "../../../assets/lemmon.jpg"
  import naimiris from "../../../assets/nai_miris.jpg"
 import rosa from "../../../assets/rosa_flower.jpg"
+import mango from "../../../assets/product_img/mango1.jpg"
+import papaya from "../../../assets/product_img/papaya.jpg"
+import grapes from "../../../assets/product_img/grapes.jpeg"
+import orange from "../../../assets/product_img/orange.jpg"
+import dragon from "../../../assets/product_img/dragon.jpg"
+import guava from "../../../assets/product_img/guava.png"
+import nelli from "../../../assets/product_img/nelli.jpg"
+import rambutan from "../../../assets/product_img/rambutan.jpg"
+import delum from "../../../assets/product_img/delum.jpeg"
 
 const product_card =[
     {
         id:1,
-        product_name:"Cane Plam ",
-        description:"12 in Stock",
-        price:1350,
+        product_name:"Mango Plant",
+        description:"60 in Stock",
+        price:150,
         currency: "LKR",
         reorderlevel:10,
         qty:12,
-        thumb:cane_plam
+        thumb:mango
     },
     {
         id:2,
-        product_name:"Sensaveria",
-        description:"8 in Stock",
-        price:250,
+        product_name:"Red Papaya Plant",
+        description:"50 in Stock",
+        price:100,
         currency:"LKR",
         reorderlevel:15,
         qty:8,
-        thumb:sensavaria
+        thumb:papaya
     },
     {
         id:3,
-        product_name:"Raphis Plam",
-        description:"5 in Stock",
-        price:1450,
+        product_name:"Grapes Plant",
+        description:"25 in Stock",
+        price:1000,
         currency:"LKR",
         reorderlevel:12,
         qty:5,
-        thumb:raphis
+        thumb:grapes
     },
     {
         id:4,
-        product_name:"Curry Leaves",
-        description:"2 in Stock",
-        price:350,
+        product_name:"Baby Orange Plant",
+        description:"20 in Stock",
+        price:1500,
         currency:"LKR",
         reorderlevel:20,
         qty:40,
-        thumb:curry_leaves
+        thumb:orange
+
     },
     {
         id:5,
-        product_name:"Budded Avcado",
-        description:"10 in Stock",
+        product_name:"Avcado Plant",
+        description:"20 in Stock",
         price:450,
         currency:"LKR",
         reorderlevel:35,
@@ -67,74 +71,53 @@ const product_card =[
     },
     {
         id:6,
-        product_name:"Cristina",
-        description:"7 in Stock",
-        price:450,
+        product_name:"Hybrid Dragon Fruit",
+        description:"10 in Stock",
+        price:2000,
         currency:"LKR",
         reorderlevel:15,
         qty:7,
-        thumb:cristina
+        thumb:dragon
     },
     {
         id:7,
-        product_name:"Rosa Pichcha",
-        description:"14 in Stock",
-        price:550,
+        product_name:"Guava Plant",
+        description:"50 in Stock",
+        price:100,
         currency: "LKR",
         reorderlevel:5,
         qty:14,
-        thumb:rosa_pichcha
+        thumb:guava
     },
     {
         id:8,
-        product_name:"Plastic Pot",
-        description:"32 in Stock",
-        price:150,
+        product_name:"Nelli Plant",
+        description:"30 in Stock",
+        price:500,
         currency:"LKR",
         reorderlevel:15,
         qty:32,
-        thumb:plastic_pot
-    },
-    {
-        id:9,
-        product_name:"Nelli Plant",
-        description:"14 in Stock",
-        price:450,
-        currency:"LKR",
-        reorderlevel:15,
-        qty:14,
         thumb:nelli
     },
     {
         id:10,
-        product_name:"Lemmon Plant",
-        description:"15 in Stock",
+        product_name:"Rambutan Plant",
+        description:"50 in Stock",
         price:350,
         currency:"LKR",
         reorderlevel:30,
         qty:15,
-        thumb:lemmon
+        thumb:rambutan
     },
     {
         id:11,
-        product_name:"Nai Miris",
-        description:"16 in Stock",
+        product_name:"Pomegranate Plant",
+        description:"25 in Stock",
         price:450,
         currency:"LKR",
         reorderlevel:25,
         qty:16,
-        thumb:naimiris
+        thumb:delum
     },
-    {
-        id:12,
-        product_name:"Red Rose",
-        description:"20 in Stock",
-        price:850,
-        currency:"LKR",
-        reorderlevel:8,
-        qty:20,
-        thumb:rosa
-    }
-     
 ]
 export default product_card;
