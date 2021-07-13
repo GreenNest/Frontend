89f91a5e21176a885e3cb15d3d603ad0099166d1
sulakshanee <sulakshanee111@gmail.com>
2021-07-13 13:03:08 +0530

    css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -44,12 +44,12 @@ import Updatestock from './pages/admin/Stock/Updatestock';
 ReactDOM.render(
   <React.StrictMode>
     <Router> 
-      {/* <Header/> */}
+      <Header/>
        {/* <AdminSidebar />   */}
        {/* <ModeratorSidebar />  */}
-      <AccountantSidebar /> 
+      {/* <AccountantSidebar />  */}
         <App/>  
-      {/* <Footer/> */}
+      <Footer/>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
