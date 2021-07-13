efe2b6988cd1de349056afda8e0046a3787f0deb
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-07-14 02:03:12 +0530

    sidebar edited

--- frontend/src/pages/moderator/components/moderatorSidebarData.js
+++ frontend/src/pages/moderator/components/moderatorSidebarData.js
@@ -73,5 +73,11 @@ export const SidebarData = [
         icon: <AiIcons.AiOutlineBarChart className="w-5 h-5 mr-3 mt-0.5"/>,
         // style: "hover:bg-gray-400 bg-gray-300 hover:text-white flex font-bold p-3 rounded w-48",
     },
+    {
+        title: 'Logout',
+        path: '#',
+        icon: <AiIcons.AiOutlineLogout className="w-5 h-5 mr-3 mt-0.5"/>,
+        // style: "hover:bg-yellow-400 bg-yellow-200 hover:text-white flex font-bold p-3 rounded w-48",
+    },
 
 ]
